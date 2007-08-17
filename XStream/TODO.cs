09d77c1aee5c83d27de402b2c239014f543fb4f6
srshti <srshti@c6ad49c3-dc33-0410-8d8f-25f6e8382c60>
2007-08-17 17:53:19 +0000

    works with multi-dimensional arrays
    git-svn-id: http://extreme.googlecode.com/svn/trunk@25 c6ad49c3-dc33-0410-8d8f-25f6e8382c60

--- XStream/TODO.cs
+++ XStream/TODO.cs
@@ -2,7 +2,6 @@ namespace XStream {
     public class TODO {
         //todo:array xmlstack to use in the references attribute
         //todo:handle other implementations of list
-        //todo:multi-dimensional arrays
         //todo:make remove IsAssignableFrom from TypeConverter
         //todo:clean up hashtable conversion
     }
