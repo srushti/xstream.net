213ff83bd849166da7b12620f1cfa4df3d637ef8
srshti <srshti@c6ad49c3-dc33-0410-8d8f-25f6e8382c60>
2008-05-09 05:42:17 +0000

    fixed array serialisation
    git-svn-id: http://extreme.googlecode.com/svn/trunk@47 c6ad49c3-dc33-0410-8d8f-25f6e8382c60

--- XStream/Converters/Collections/ArrayConverter.cs
+++ XStream/Converters/Collections/ArrayConverter.cs
@@ -2,6 +2,8 @@ using System;
 
 namespace xstream.Converters.Collections {
     internal class ArrayConverter : Converter {
+        private const string ARRAY_TYPE = "array-type";
+
         public bool CanConvert(Type type) {
             return type.IsArray;
         }
@@ -9,14 +11,16 @@ namespace xstream.Converters.Collections {
         public void ToXml(object value, XStreamWriter writer, MarshallingContext context) {
             Array array = (Array) value;
             string typeName = value.GetType().AssemblyQualifiedName;
-            writer.WriteAttribute("array-type", typeName.Substring(0, typeName.LastIndexOf("[]")));
+            int lastIndexOfBrackets = typeName.LastIndexOf("[]");
+            string arrayType = string.Concat(typeName.Substring(0, lastIndexOfBrackets), typeName.Substring(lastIndexOfBrackets + 2));
+            writer.WriteAttribute(ARRAY_TYPE, arrayType);
             foreach (object o in array)
                 context.ConvertOriginal(o);
         }
 
         public object FromXml(XStreamReader reader, UnmarshallingContext context) {
             int count = reader.NoOfChildren();
-            Array result = Array.CreateInstance(context.GetTypeFromOtherAssemblies(reader.GetAttribute("array-type")), count);
+            Array result = Array.CreateInstance(context.GetTypeFromOtherAssemblies(reader.GetAttribute(ARRAY_TYPE)), count);
             reader.MoveDown();
             for (int i = 0; i < count; i++) {
                 result.SetValue(context.ConvertOriginal(), i);
