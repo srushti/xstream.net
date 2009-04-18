7bd37a28fdd1954a549ade913795812dd1138e6f
Srushtivratha Ambekallu <srushti@newkrypton.corporate.thoughtworks.com>
2009-04-18 14:54:01 +0530

    actually calling the compile

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,8 +5,14 @@ require 'CSProjFile.rb'
 task :default => [:compile, :test]
 
 task :compile do
-  CSProjFile.new('XStream/XStream.csproj')
+  csproj_file = CSProjFile.new('XStream/XStream.csproj')
+  fail_on_error (csproj_file.create_csc, "compile")
 end
 
 task :test do
+end
+
+def fail_on_error(command, name)
+  task_successful = system command
+  raise ("Command " + name + " failed") if !task_successful
 end
